338771d050e6aa1752602e996b9ab4b325fc2d42
Ian Wahbe <me@iwahbe.com>
2024-10-11 00:54:43 +0200

    Support GNU extensions

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@ mod tests {
         unsafe { assert!(FNM_NOESCAPE > 0 || FNM_NOESCAPE <= 0) };
         unsafe { assert!(FNM_PATHNAME > 0 || FNM_PATHNAME <= 0) };
         unsafe { assert!(FNM_PERIOD > 0 || FNM_PERIOD <= 0) };
+        #[cfg(feature = "gnu")]
+        unsafe {
+            assert!(FNM_CASEFOLD > 0 || FNM_CASEFOLD <= 0);
+            assert!(FNM_EXTMATCH > 0 || FNM_EXTMATCH <= 0);
+        }
         let _ = |pattern: *const c_char, string: *const c_char, flags: c_int| unsafe {
             fnmatch(pattern, string, flags)
         };
@@ -34,12 +39,42 @@ extern "C" {
     /// The flags argument modifies the behavior; it is the bitwise OR of zero
     /// or more of the following flags:
     ///
-    ///     `FNM_NOESCAPE`
-    ///     `FNM_PATHNAME`
-    ///     `FNM_PERIOD`
+    /// - `FNM_NOESCAPE`
+    /// - `FNM_PATHNAME`
+    /// - `FNM_PERIOD`
+    /// - `FNM_CASEFOLD` (feature = "gnu")
+    /// - `FNM_EXTMATCH` (feature = "gnu")
     ///
     /// RETURN VALUE
-    /// Zero if string matches pattern, FNM_NOMATCH if there is no match or
+    /// Zero if string matches pattern, `FNM_NOMATCH` if there is no match or
     /// another nonzero value if there is an error.
     pub fn fnmatch(pattern: *const c_char, string: *const c_char, flags: c_int) -> c_int;
+}
+
+// GNU Extensions to fnmatch.h
+#[cfg(feature = "gnu")]
+extern "C" {
+    /// Case insensitive search.
+    #[link_name = "fnm_casefold"]
+    pub static FNM_CASEFOLD: c_int;
+    /// If this flag (a GNU extension) is set, extended patterns are supported, as
+    /// introduced by 'ksh' and now supported by other shells.  The extended format is as
+    /// follows, with pattern-list being a '|' separated list of patterns.
+    ///
+    /// '?(pattern-list)' - The pattern matches if zero or one occurrences of any of the
+    /// patterns in the pattern-list match the input string.
+    ///
+    /// '*(pattern-list)' - The pattern matches if zero or more occurrences of any of the
+    /// patterns in the pattern-list match the input string.
+    ///
+    /// '+(pattern-list)' - The pattern matches if one or more occurrences of any of the
+    /// patterns in the pattern-list match the input string.
+    ///
+    /// '@(pattern-list)' - The pattern matches if exactly one occurrence of any of the
+    /// patterns in the pattern-list match the input string.
+    ///
+    /// '!(pattern-list)' - The pattern matches if the input string cannot be matched with
+    /// any of the patterns in the pattern-list.
+    #[link_name = "fnm_extmatch"]
+    pub static FNM_EXTMATCH: c_int;
 }
--- src/link.c
+++ src/link.c
@@ -5,3 +5,13 @@ int fnm_nomatch = FNM_NOMATCH; /* Match failed. */
 int fnm_noescape = FNM_NOESCAPE; /* Disable backslash escaping. */
 int fnm_pathname = FNM_PATHNAME; /* Slash must be matched by slash. */
 int fnm_period = FNM_PERIOD;     /* Period must be matched by period. */
+
+/* A GNU extension: the pattern is matched ignoring a case */
+#ifdef FNM_CASEFOLD
+int fnm_casefold = FNM_CASEFOLD;
+#endif
+
+/* A GNU extension: extended patters are supported */
+#ifdef FNM_EXTMATCH
+int fnm_extmatch = FNM_EXTMATCH;
+#endif
